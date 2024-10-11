7362213da8d207166a67608bfe61bfabb70392ce
Swarnab-Code <swarnab2211@gmail.com>
2024-10-11 18:24:53 +0530

    Castling Error Fix

--- src/board.py
+++ src/board.py
@@ -44,7 +44,7 @@ class Board :
 
 		# king castling
 		if isinstance(piece, King) :
-			if self.castling(initial, final) :
+			if self.castling(initial, final) and not testing :
 				difference = final.col - initial.col
 				rook = piece.left_rook if (difference < 0) else piece.right_rook
 				self.move(rook, rook.moves[-1])
